075ac324a5011546dc2b6998514c7b21f34fe3a1
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-28 14:54:12 -0300

    Requisito 6 - Cria model para obter todas as vendas

--- models/saleModel.js
+++ models/saleModel.js
@@ -12,7 +12,19 @@ const addSaleProducts = async ({ saleId, productId, quantity }) => {
   );
 };
 
+const getAll = async () => {
+  const [sales] = await connection.execute(
+    `SELECT sales.id AS saleId, sales.date, salesproducts.product_id,salesproducts.quantity 
+    FROM StoreManager.sales_products AS salesproducts JOIN
+      StoreManager.sales AS sales
+      ON salesproducts.sale_id = sales.id`,
+  );
+
+  return sales;
+};
+
 module.exports = { 
   createNewSale,
   addSaleProducts,
+  getAll,
 };
