fc2afeff5a9c247dcf7f780d7fb942ce3959b7b7
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-28 17:52:21 -0300

    Refatora logica de validação de id de produto

--- middlewares/validateSaleProductId.js
+++ middlewares/validateSaleProductId.js
@@ -1,10 +1,7 @@
 const validateSaleProductId = (req, res, next) => {
   const sales = req.body;
-  sales.forEach((sale) => {
-    if (!('product_id' in sale)) {
-      return res.status(400).json({ message: '"product_id" is required' });        
-    }
-  });
+  const someInvalid = sales.some((sale) => !('product_id' in sale));
+  if (someInvalid) return res.status(400).json({ message: '"product_id" is required' }); 
 
   next();
 };
