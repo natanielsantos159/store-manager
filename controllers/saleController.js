62238377b9b5e1406f1e52a8ca90bb20e6998f69
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-28 16:31:04 -0300

    Requisito 6 - Cria controller para obter vendas pelo id

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -19,7 +19,19 @@ const getAll = async (req, res) => {
   return res.status(200).json(sales);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) return res.status(404).json({ message: 'Sale not found' });
+
+  const sale = await saleService.getById(id);
+  if (sale && sale.length) return res.status(200).json(sale);
+
+  return res.status(404).json({ message: 'Sale not found' });
+};
+
 module.exports = {
   addSales,
   getAll,
+  getById,
 };
