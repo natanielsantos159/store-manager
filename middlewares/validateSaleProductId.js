84f5f0911d6ca188abbb2cd0883393a169b5963d
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-29 17:20:12 -0300

    Refatora validação de id de produto da venda

--- middlewares/validateSaleProductId.js
+++ middlewares/validateSaleProductId.js
@@ -1,5 +1,8 @@
 const validateSaleProductId = (req, res, next) => {
   const sales = req.body;
+  if (!(sales instanceof Array)) {
+    return res.status(400).json({ message: '"product_id" is required' });
+  }
   const someInvalid = sales.some((sale) => !('product_id' in sale));
   if (someInvalid) return res.status(400).json({ message: '"product_id" is required' }); 
 
