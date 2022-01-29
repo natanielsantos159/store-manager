431d46dd98bc40bf48848f4fe4056d7b73bbcee4
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-29 16:02:00 -0300

    Requisito 8 - Implementa testes da função productService.getAll

---
+++ test/unit/services.js
@@ -0,0 +1,63 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const productModel = require('../../models/productModel');
+const saleModel = require('../../models/saleModel');
+
+const productService = require('../../services/productService');
+const saleService = require('../../services/saleService');
+
+describe('Testa o arquivo productService', () => {
+
+  describe('função getAll', () => {
+    
+    before(async () => {
+      const response =   [
+        {
+          id: 1,
+          name: "produto A",
+          quantity: 10
+        },
+        {
+          id: 2,
+          name: "produto B",
+          quantity: 20
+        }
+      ];
+      sinon.stub(productModel, 'getAll').resolves(response);
+    })
+
+    after(async () => {
+      productModel.getAll.restore();
+    })
+
+    it('deve retornar um array', async () => {
+      const response = await productService.getAll();
+      expect(response).to.be.an('array');
+    })
+
+    it('o array deve conter apenas objetos', async () => {
+      const response = await productService.getAll();
+      response.forEach((el) => {
+        expect(el).to.be.an('object');
+      })
+    })
+
+    it('os objetos devem ter as propriedades esperadas', async () => {
+      const response = await productService.getAll();
+      response.forEach((el) => {
+        expect(el).to.have.all.keys(['id', 'name', 'quantity']);
+      })
+    })
+
+    it('os objetos devem ter as propriedades com os tipos esperados', async () => {
+      const response = await productService.getAll();
+      response.forEach((el) => {
+        expect(el.id).to.be.an('number');
+        expect(el.name).to.be.an('string');
+        expect(el.quantity).to.be.an('number');
+      })
+    })
+  });
+  
+})
