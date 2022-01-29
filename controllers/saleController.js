f112d57d4b9f7d22c93bf588ccdd91ecf6163bcf
Nataniel Santos <nathan.santos159@hotmail.com>
2022-01-28 21:03:59 -0300

    Requisito 7 - Cria controller para obter venda pelo id

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -4,11 +4,11 @@ const productService = require('../services/productService');
 const addSales = async (req, res) => {
   const sales = req.body;
 
-  const validIds = sales.every(async ({ product_id }) => {
-    const result = await productService.productsExists(product_id);
-    return result;
+  const promisesArray = sales.map(({ product_id }) => productService.productsExists(product_id));
+  Promise.all(promisesArray).then((productIds) => {
+    const allValids = productIds.every((isValid) => isValid);
+    if (!allValids) res.status(404).json({ message: 'Product not found' });
   });
-  if (!validIds) res.status(404).json({ message: 'Product not found' });
 
   const addedSaleId = await saleService.addSales(sales);
   res.status(201).json({ id: addedSaleId, itemsSold: sales });
@@ -30,8 +30,22 @@ const getById = async (req, res) => {
   return res.status(404).json({ message: 'Sale not found' });
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+  const [sale] = req.body;
+
+  const productExists = await productService.getById(sale.product_id);
+  if (!productExists) return res.status(404).json({ message: 'Sale not found' });
+  await saleService.update({ id, ...sale });
+
+  return res.status(200).json(
+    { saleId: id, itemUpdated: [{ id, ...sale }] },
+  );
+};
+
 module.exports = {
   addSales,
   getAll,
   getById,
+  update,
 };
